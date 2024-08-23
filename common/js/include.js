b6590da192e981efcfd3f4cd65481c33b08694cd
sohee <sweetbox95@naver.com>
2024-08-23 23:37:57 +0900

    css 적용 테스트

--- common/js/include.js
+++ common/js/include.js
@@ -1,8 +1,8 @@
 document.write(`
     <!-- fontawesome -->
-    <script src="common/plugin/fontawesome-free-6.1.2-web/js/all.js"></script>
+    <script src="./common/plugin/fontawesome-free-6.1.2-web/js/all.js"></script>
     
     <!-- common -->
-    <script src="/common/js/common.js"></script>
-    <link rel="stylesheet" href="/common/css/app.css" type="text/css"/>
+    <script src="./common/js/common.js"></script>
+    <link rel="stylesheet" href="./common/css/app.css" type="text/css"/>
 `)
